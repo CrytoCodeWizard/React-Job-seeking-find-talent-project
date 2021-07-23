6ca8d0e09fc9a2deecaac87963ddb4ba3deb5d08
David Teo <starrynightskyhoney@gmail.com>
2021-07-23 17:56:16 +0530

    recruiterPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import Recruit from "./Components/Recruit/Recruit";
-
+import LandingPage from "./Components/LandingPage/LandingPage";
+import { AngelList } from "./Components/Jobs/AngelList";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 function App() {
   return (
-    <Recruit />
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact component={LandingPage} />
+          <Route path="/jobs" component={AngelList} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
